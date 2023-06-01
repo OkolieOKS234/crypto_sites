169ca42595e0cce199c9905db18aa1323341f81d
OkolieOKS234 <dokolie040@gmail.com>
2023-06-02 00:27:28 +0100

    removed snow

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Snowfall from 'react-snowfall'
+// import Snowfall from 'react-snowfall'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <Snowfall 
-    
-     />
+     {/* <Snowfall /> */}
     <App />
   </React.StrictMode>,
 )
