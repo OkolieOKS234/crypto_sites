83ce5efca3fd08f771364d385657b62c1d48513d
OkolieOKS234 <dokolie040@gmail.com>
2023-06-15 15:42:43 +0100

    added slider to make the site more user-friendly

--- src/components/Faucets.jsx
+++ src/components/Faucets.jsx
@@ -4,12 +4,56 @@ import Cryptoflare from "../assets/sites/cryptoflare.jpg"
 import Coinpayu from "../assets/sites/coinpayu.jpg"
 import Earnviv from "../assets/sites/earnviv.jpg"
 import EarnBitMoon from "../assets/sites/viefaucet.jpg"
+import { useState, useEffect } from 'react';
+import SwiperCore, { Navigation, Pagination, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const Faucets = () => {
+// Width Checker
+const [slidesPerView, setSlidesPerView] = useState(3);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setSlidesPerView(1);
+      } else if (window.innerWidth <= 1024) {
+        setSlidesPerView(2);
+      } else {
+        setSlidesPerView(3);
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+
+
   return (
     <>
 <div className='mx-9 sites flex flex-wrap gap-4 py-3'>
+<Swiper
+      // install Swiper modules
+      modules={[Navigation, Pagination,  A11y]}
+      spaceBetween={50}
+      slidesPerView={slidesPerView}
+      navigation
+      pagination={{ clickable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+    >
+
                 {/* First Card */}
+                <SwiperSlide>
                 <Reveal triggerOnce={true}>
         <div className=' bg-slate-950 shadow-lg pb-3 hover:scale-110 rounded-xl'>
             <div className='h-[580px] w-[350px]  ' >
@@ -30,13 +74,14 @@ const Faucets = () => {
             </div>
             </div>
             </Reveal>
-
+            </SwiperSlide>
             
 {/* Second */}
+<SwiperSlide>
 <Reveal triggerOnce={true}>
         <div className=' bg-slate-950 shadow-lg pb-3 hover:scale-110 rounded-xl'>
             <div className='h-[580px] w-[350px]  ' >
-            <img src={Cryptoflare} alt=""  className=' w-56 m-auto object-contain shadow-2xl shadow-red-600 pt-6'/>
+            <img src={Cryptoflare} alt=""  className=' w-56 m-auto object-contain shadow-2xl shadow-red-600 pt-6 '/>
             <h3 className='text-2xl text-white text-center '>Cryptoflare</h3>
             <ul className='text-white list-none text-center'>
               <li className='list-inside flex mx-12' >Claim every 15 minutes</li>
@@ -53,7 +98,9 @@ const Faucets = () => {
             </div>
             </div>
             </Reveal>
+            </SwiperSlide>
 {/* Third */}
+<SwiperSlide>
 <Reveal triggerOnce={true}>
         <div className=' bg-slate-950 shadow-lg pb-3 hover:scale-110 rounded-xl'>
             <div className='h-[580px] w-[350px]  ' >
@@ -74,7 +121,9 @@ const Faucets = () => {
             </div>
             </div>
             </Reveal>
+            </SwiperSlide>
             {/* fOURTH */}
+            <SwiperSlide>
             <Reveal triggerOnce={true}>
         <div className=' bg-slate-950 shadow-lg pb-3 hover:scale-110 rounded-xl'>
             <div className='h-[580px] w-[350px]  ' >
@@ -95,7 +144,9 @@ const Faucets = () => {
             </div>
             </div>
             </Reveal>
+            </SwiperSlide>
 {/* Fifth */}
+<SwiperSlide>
 <Reveal triggerOnce={true}>
         <div className=' bg-slate-950 shadow-lg pb-3 hover:scale-110 rounded-xl'>
             <div className='h-[580px] w-[350px]'>
@@ -116,7 +167,9 @@ const Faucets = () => {
             </div>
             </div>
             </Reveal>
+            </SwiperSlide>
 {/* Sixth */}
+<SwiperSlide>
 <Reveal triggerOnce={true}>
         <div className=' bg-slate-950 shadow-lg pb-3 hover:scale-110 rounded-xl'>
             <div className='h-[580px] w-[350px]  ' >
@@ -137,8 +190,9 @@ const Faucets = () => {
             </div>
             </div>
             </Reveal>
-
+            </SwiperSlide>
             {/* Seventh */}
+            <SwiperSlide>
             <Reveal triggerOnce={true}>
         <div className=' bg-slate-950 shadow-lg pb-3 hover:scale-110 rounded-xl'>
             <div className='h-[580px] w-[350px]  ' >
@@ -159,7 +213,9 @@ const Faucets = () => {
             </div>
             </div>
             </Reveal>
+            </SwiperSlide>
 {/* Eigth */}
+<SwiperSlide>
 <Reveal triggerOnce={true}>
         <div className=' bg-slate-950 shadow-lg pb-3 hover:scale-110 rounded-xl'>
             <div className='h-[580px] w-[350px]  ' >
@@ -180,8 +236,9 @@ const Faucets = () => {
             </div>
             </div>
             </Reveal>
-
+            </SwiperSlide>
             {/* Ninth */}
+            <SwiperSlide>
             <Reveal triggerOnce={true}>
         <div className=' bg-slate-950 shadow-lg pb-3 hover:scale-110 rounded-xl'>
             <div className='h-[580px] w-[350px]  ' >
@@ -202,8 +259,9 @@ const Faucets = () => {
             </div>
             </div>
             </Reveal>
-
+            </SwiperSlide>
             {/* Tenth */}
+            <SwiperSlide>
             <Reveal triggerOnce={true}>
         <div className=' bg-slate-950 shadow-lg pb-3 hover:scale-110 rounded-xl'>
             <div className='h-[580px] w-[350px]  ' >
@@ -224,8 +282,9 @@ const Faucets = () => {
             </div>
             </div>
             </Reveal>
-
+            </SwiperSlide>
             {/* Eleventh */}
+            <SwiperSlide>
             <Reveal triggerOnce={true}>
         <div className=' bg-slate-950 shadow-lg pb-3 hover:scale-110 rounded-xl'>
             <div className='h-[580px] w-[350px]  ' >
@@ -246,8 +305,9 @@ const Faucets = () => {
             </div>
             </div>
             </Reveal>
-
+            </SwiperSlide>
             {/* Twelfth */}
+            <SwiperSlide>
             <Reveal triggerOnce={true}>
         <div className=' bg-slate-950 shadow-lg pb-3 hover:scale-110 rounded-xl'>
             <div className='h-[580px] w-[350px]  ' >
@@ -268,8 +328,9 @@ const Faucets = () => {
             </div>
             </div>
             </Reveal>
-
+            </SwiperSlide>
             {/* Thirteen */}
+            <SwiperSlide>
             <Reveal triggerOnce={true}>
         <div className=' bg-slate-950 shadow-lg pb-3 hover:scale-110 rounded-xl'>
             <div className='h-[580px] w-[350px]  ' >
@@ -290,8 +351,10 @@ const Faucets = () => {
             </div>
             </div>
             </Reveal>
+            </SwiperSlide>
 {/* 14th */}
-<Reveal triggerOnce={true}>
+<SwiperSlide>
+  <Reveal triggerOnce={true}>
         <div className=' bg-slate-950 shadow-lg pb-3 hover:scale-110 rounded-xl'>
             <div className='h-[580px] w-[350px]  ' >
             <img src={EarnBitMoon} alt=""  className=' w-56 m-auto object-contain shadow-2xl pt-6 shadow-red-600'/>
@@ -311,8 +374,11 @@ const Faucets = () => {
             </div>
             </div>
             </Reveal>
+            </SwiperSlide>
 
+            </Swiper>
             </div>
+            
 
     </>
   )
