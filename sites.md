b1415fb8e7ccf0df4672b8460cc4457938b4e4e8
OkolieOKS234 <dokolie040@gmail.com>
2023-07-27 16:16:24 +0100

    added some tweaks

---
+++ sites.md
@@ -0,0 +1,10 @@
+Faucets
+- COinpayu
+- Viefaucet
+- Dutchycorp
+- Cryptoflare
+
+Gaming
+- Wombat
+- Rollercoin
+- 
---
+++ src/data/gaming.js
@@ -0,0 +1,33 @@
+// https://res.cloudinary.com/dsx2yofap/image/upload/v1690470152/rollercoin_k5xcvb.jpg
+
+const gamingData = [
+    {
+      index: 1,
+      nameOfSite: "Rollercoin",
+      image: "https://res.cloudinary.com/dsx2yofap/image/upload/v1690470152/rollercoin_k5xcvb.jpg",
+description:"The RollerCoin is a blockchain-based game platform that allows players to earn"
+        
+    },
+    {
+      index: 2,
+      nameOfSite: "Earnviv",
+      image: "https://res.cloudinary.com/dsx2yofap/image/upload/v1690470581/earnviv_dwpf5g.jpg",
+      description:"EarnViv offers the opportunity to earn crypto playing html based games"
+    },
+    {
+        index: 3,
+        nameOfSite: "Wombat",
+        image: "https://res.cloudinary.com/dsx2yofap/image/upload/v1690470774/womplay_vnqpww.jpg",
+        description:"Wombat Game Studio provides an online multiplayer browser game where users compete for"
+      },
+  
+      {
+        index: 4,
+        nameOfSite: "dutchy",
+        image: "https://res.cloudinary.com/dsx2yofap/image/upload/v1690470926/dutchy_xkj7jf.jpg",
+        description:"DutchY is a faucet that offers 2000+ games to play and earn."
+      },
+
+    // You can add more objects here to simulate more API data
+  ];
+  export default gamingData;
